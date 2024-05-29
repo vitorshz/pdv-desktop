af424218fc9bf48d69b2523dc16560e7c55d7f73
vitorshz <110640686+vitorshz@users.noreply.github.com>
2024-05-29 15:42:41 -0300

    Update VendaView.java

--- pdv-desktop/src/main/java/br/unipar/views/VendaView.java
+++ pdv-desktop/src/main/java/br/unipar/views/VendaView.java
@@ -18,9 +18,16 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class VendaView extends JFrame {
     private JTable clientesTable;
@@ -28,6 +35,10 @@ public class VendaView extends JFrame {
     private JTable itensVendaTable;
     private JTextField observacoesField;
     private JTextField quantidadeField;
+    private JTextField totalField;
+    private JTextField produtoField;
+    private JTextField precoField;
+    private JTextField descricaoField;
     private JButton adicionarItemButton;
     private JButton realizarVendaButton;
     private JButton atualizarClientesButton;
@@ -36,70 +47,125 @@ public class VendaView extends JFrame {
     private List<Cliente> clientes;
     private List<Produto> produtos;
     private DefaultTableModel itensVendaModel;
+    private JLabel totalVendaLabel;
 
     public VendaView() {
-        apiService = RetrofitConfig.getApiService();
         setTitle("Ponto de Venda");
         setSize(1200, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        // Painel de Clientes
-        JPanel clientesPanel = new JPanel(new BorderLayout());
-        clientesPanel.setBorder(BorderFactory.createTitledBorder("Clientes"));
+        // Painel de Controle Superior
+        JPanel controlPanel = new JPanel(new GridLayout(1, 3, 10, 10));
+
+
+        // Painel de Clientes e Vendedor
+        JPanel clienteVendedorPanel = new JPanel(new GridLayout(1, 2, 10, 10));
+
+        // Tamanho máximo da tabela de clientes
+        int maxHeight = 100;
+
+// Painel de Clientes
+        JPanel clientePanel = new JPanel(new BorderLayout());
+        clientePanel.setBorder(BorderFactory.createTitledBorder("Clientes"));
+        clientePanel.add(new JLabel("Clientes"), BorderLayout.NORTH);
         clientesTable = new JTable();
-        clientesPanel.add(new JScrollPane(clientesTable), BorderLayout.CENTER);
+        clientesTable.setPreferredScrollableViewportSize(new Dimension(clientesTable.getPreferredSize().width, maxHeight)); // Define a altura máxima da tabela de clientes
+        clientePanel.add(new JScrollPane(clientesTable), BorderLayout.CENTER);
+        clientePanel.setPreferredSize(new Dimension(300, 100)); // Definindo um tamanho menor
+        clienteVendedorPanel.add(clientePanel);
 
-        atualizarClientesButton = new JButton("Atualizar Clientes", new ImageIcon("path_to_refresh_icon"));
-        atualizarClientesButton.addActionListener(e -> listarClientes());
-        clientesPanel.add(atualizarClientesButton, BorderLayout.SOUTH);
 
-        // Painel de Produtos
+// Painel de Produtos
         JPanel produtosPanel = new JPanel(new BorderLayout());
         produtosPanel.setBorder(BorderFactory.createTitledBorder("Produtos"));
+        produtosPanel.add(new JLabel("Produtos"), BorderLayout.NORTH);
         produtosTable = new JTable();
-        produtosPanel.add(new JScrollPane(produtosTable), BorderLayout.CENTER);
+        produtosTable.setPreferredScrollableViewportSize(new Dimension(produtosTable.getPreferredSize().width, maxHeight)); // Define a altura máxima da tabela de produtos
+        produtosPanel.add(new JScrollPane(produtosTable), BorderLayout.CENTER); // Alterado de 'vendedorPanel' para 'produtoPanel'
+        produtosPanel.setPreferredSize(new Dimension(300, 100)); // Definindo um tamanho menor
+        clienteVendedorPanel.add(produtosPanel);
 
-        atualizarProdutosButton = new JButton("Atualizar Produtos", new ImageIcon("path_to_refresh_icon"));
-        atualizarProdutosButton.addActionListener(e -> listarProdutos());
-        produtosPanel.add(atualizarProdutosButton, BorderLayout.SOUTH);
 
-        // Painel de Venda
-        JPanel vendaPanel = new JPanel(new GridLayout(3, 2, 10, 10));
-        vendaPanel.setBorder(BorderFactory.createTitledBorder("Detalhes da Venda"));
 
-        vendaPanel.add(new JLabel("Observações:"));
-        observacoesField = new JTextField();
-        vendaPanel.add(observacoesField);
+// Painel de Itens da Venda
+        itensVendaModel = new DefaultTableModel(new Object[]{"Produto", "Quantidade", "Valor Unitário", "Valor Total"}, 0);
+        itensVendaTable = new JTable(itensVendaModel);
+        JPanel iitensVendaPanel = new JPanel(new BorderLayout());
+        iitensVendaPanel.setBorder(BorderFactory.createTitledBorder("Itens de Venda"));
+        iitensVendaPanel.add(new JScrollPane(itensVendaTable), BorderLayout.CENTER);
+        iitensVendaPanel.setPreferredSize(new Dimension(600, 200)); // Definindo um tamanho para o painel de itens de venda
 
-        vendaPanel.add(new JLabel("Quantidade:"));
+
+
+        // Painel de Total da Venda
+        JPanel totalVendaPanel = new JPanel(new BorderLayout());
+        totalVendaPanel.setBorder(BorderFactory.createTitledBorder("Total Venda R$"));
+        totalVendaLabel = new JLabel("0,00", SwingConstants.CENTER);
+        totalVendaLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        totalVendaPanel.add(totalVendaLabel, BorderLayout.CENTER);
+
+        // Painel de Produto
+        JPanel produtoPanel = new JPanel(new GridLayout(4, 2, 10, 10));
+        produtoPanel.setBorder(BorderFactory.createTitledBorder("Produto"));
+
+        // Define o tamanho máximo para os campos de texto
+        int maxTextFieldWidth = 100; // Escolha o valor máximo que deseja para a largura dos campos de texto
+
+        produtoPanel.add(new JLabel("Produto:"));
+        produtoField = new JTextField();
+        produtoPanel.add(produtoField);
+
+        produtoPanel.add(new JLabel("Qtde:"));
         quantidadeField = new JTextField();
-        vendaPanel.add(quantidadeField);
+        produtoPanel.add(quantidadeField);
 
-        adicionarItemButton = new JButton("Adicionar Item", new ImageIcon("path_to_plus_icon"));
-        vendaPanel.add(adicionarItemButton);
+        produtoPanel.add(new JLabel("Preço:"));
+        precoField = new JTextField();
+        produtoPanel.add(precoField);
 
-        realizarVendaButton = new JButton("Realizar Venda", new ImageIcon("path_to_cart_icon"));
-        vendaPanel.add(realizarVendaButton);
+        produtoPanel.add(new JLabel("Desc:"));
+        descricaoField = new JTextField();
+        produtoPanel.add(descricaoField);
 
-        // Tabela de Itens da Venda
+        // Define o tamanho máximo para os campos de texto de produto
+        produtoField.setMaximumSize(new Dimension(maxTextFieldWidth, produtoField.getPreferredSize().height));
+        quantidadeField.setMaximumSize(new Dimension(maxTextFieldWidth, quantidadeField.getPreferredSize().height));
+        precoField.setMaximumSize(new Dimension(maxTextFieldWidth, 100));
+        descricaoField.setMaximumSize(new Dimension(maxTextFieldWidth, descricaoField.getPreferredSize().height));
+
+        // Painel de Observações
+        JPanel observacoesPanel = new JPanel(new BorderLayout());
+        observacoesPanel.setBorder(BorderFactory.createTitledBorder("Observações"));
+        observacoesField = new JTextField();
+        observacoesPanel.add(observacoesField, BorderLayout.CENTER);
+
+        // Painel de Itens da Venda
         itensVendaModel = new DefaultTableModel(new Object[]{"Produto", "Quantidade", "Valor Unitário", "Valor Total"}, 0);
         itensVendaTable = new JTable(itensVendaModel);
         JPanel itensVendaPanel = new JPanel(new BorderLayout());
-        itensVendaPanel.setBorder(BorderFactory.createTitledBorder("Itens da Venda"));
+        itensVendaPanel.setBorder(BorderFactory.createTitledBorder("Itens / Qtde"));
         itensVendaPanel.add(new JScrollPane(itensVendaTable), BorderLayout.CENTER);
 
-        // Painel Principal
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, clientesPanel, produtosPanel);
-        splitPane.setDividerLocation(600);
+        // Painel de Ações
+        JPanel actionPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        adicionarItemButton = new JButton("Adicionar Item", new ImageIcon("path_to_add_icon"));
+        actionPanel.add(adicionarItemButton);
+        realizarVendaButton = new JButton("Realizar Venda", new ImageIcon("path_to_checkout_icon"));
+        actionPanel.add(realizarVendaButton);
 
-        add(splitPane, BorderLayout.NORTH);
-        add(vendaPanel, BorderLayout.CENTER);
+        // Adicionando painéis ao frame principal
+        add(controlPanel, BorderLayout.NORTH);
+        add(clienteVendedorPanel, BorderLayout.WEST);
+        add(totalVendaPanel, BorderLayout.EAST);
+        add(produtoPanel, BorderLayout.CENTER);
+        add(observacoesPanel, BorderLayout.SOUTH);
         add(itensVendaPanel, BorderLayout.SOUTH);
+        add(actionPanel, BorderLayout.SOUTH);
 
-        // Carregar dados dos clientes e produtos
-        listarClientes();
-        listarProdutos();
+//        // Carregar dados dos clientes e produtos
+//        listarClientes();
+//        listarProdutos();
 
         // Configurar ações dos botões
         adicionarItemButton.addActionListener(new ActionListener() {
@@ -115,6 +181,17 @@ public class VendaView extends JFrame {
                 realizarVenda();
             }
         });
+
+//        // Atualizar automaticamente os clientes e produtos a cada 5 minutos
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                listarClientes();
+//                listarProdutos();
+//            }
+//        }, 0, 5 * 60 * 1000); // Atualiza a cada 5 minutos (5 * 60 * 1000 ms)
+
     }
 
     private void listarClientes() {
@@ -124,12 +201,14 @@ public class VendaView extends JFrame {
                 if (response.isSuccessful()) {
                     clientes = response.body();
                     atualizarTabelaClientes(clientes);
+                    registrarLog("Obtenção de clientes", "Sucesso");
                 }
             }
 
             @Override
             public void onFailure(Call<List<Cliente>> call, Throwable t) {
                 JOptionPane.showMessageDialog(VendaView.this, "Erro ao listar clientes.");
+                registrarLog("Obtenção de clientes", "Falha");
             }
         });
     }
@@ -141,15 +220,18 @@ public class VendaView extends JFrame {
                 if (response.isSuccessful()) {
                     produtos = response.body();
                     atualizarTabelaProdutos(produtos);
+                    registrarLog("Obtenção de produtos", "Sucesso");
                 }
             }
 
             @Override
             public void onFailure(Call<List<Produto>> call, Throwable t) {
                 JOptionPane.showMessageDialog(VendaView.this, "Erro ao listar produtos.");
+                registrarLog("Obtenção de produtos", "Falha");
             }
         });
     }
+
 
     private void atualizarTabelaClientes(List<Cliente> clientes) {
         String[] colunas = {"ID", "Nome", "Telefone", "Email"};
@@ -232,17 +314,21 @@ public class VendaView extends JFrame {
                         adicionarItemVenda(vendaCriada, i);
                     }
                     JOptionPane.showMessageDialog(VendaView.this, "Venda realizada com sucesso!");
+                    registrarLog("Inserção de venda", "Sucesso");
                 } else {
                     JOptionPane.showMessageDialog(VendaView.this, "Erro ao criar venda.");
+                    registrarLog("Inserção de venda", "Falha");
                 }
             }
 
             @Override
             public void onFailure(Call<Venda> call, Throwable t) {
                 JOptionPane.showMessageDialog(VendaView.this, "Erro ao criar venda.");
+                registrarLog("Inserção de venda", "Falha");
             }
         });
     }
+
 
     private void adicionarItemVenda(Venda venda, int rowIndex) {
         Produto produto = produtos.stream()
@@ -270,6 +356,17 @@ public class VendaView extends JFrame {
                     JOptionPane.showMessageDialog(VendaView.this, "Erro ao adicionar item de venda.");
                 }
             });
+        }
+    }
+    private void registrarLog(String operacao, String status) {
+        String logFilePath = "log.txt";
+        try (PrintWriter writer = new PrintWriter(new FileWriter(logFilePath, true))) {
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String dataHora = now.format(formatter);
+            writer.println(dataHora + " - Operação: " + operacao + " - Status: " + status);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
