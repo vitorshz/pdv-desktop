1cdb95d47ece768ef10eb94952e9a1f59d14b8cb
eduardonakaidev <109884797+eduardonakaidev@users.noreply.github.com>
2024-06-06 19:54:31 -0300

    feat: adicionei os metodos da api no Api service

--- pdv-desktop/src/main/java/br/unipar/services/ApiService.java
+++ pdv-desktop/src/main/java/br/unipar/services/ApiService.java
@@ -6,8 +6,11 @@ import br.unipar.models.Produto;
 import br.unipar.models.Venda;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 import java.util.List;
 
@@ -24,4 +27,34 @@ public interface ApiService {
 
     @POST("itens-venda")
     Call<ItemVenda> adicionarItemVenda(@Body ItemVenda itemVenda);
+    // falta ver como recuperar o id da url
+    @GET("itens-venda/venda/{id}")
+    Call<List<ItemVenda>> listarItemVendaDaVenda(@Path("id") Long id);
+    @DELETE("vendas/{id}")
+    void deletarVenda(@Path("id") Long id);
+    @PUT("vendas/{id}")
+    Call<Venda> updateVenda(@Path("id") Long id,@Body Venda venda);
+    @GET("vendas/{id}")
+    Call<Venda> getByIdVenda(@Path("id") Long id);
+    @GET("vendas")
+    Call<List<Venda>> listarVendas();
+    @DELETE("produtos/{id}")
+    void deletarProduto(@Path("id") Long id);
+    @PUT("produtos/{id}")
+    Call<Produto> updateProduto( @Path("id") Long id, @Body Produto produto);
+    @POST("produtos")
+    Call<Produto> criarProduto(@Body Produto produto);
+    @GET("produtos/{id}")
+    Call<Produto> getByIdProduto(@Path("id") Long id);
+    @DELETE("clientes/{id}")
+    void deletarCliente(@Path("id") Long id);
+    @PUT("clientes/{id}")
+    Call<Cliente> updateCliente(@Path("id") Long id, @Body Cliente cliente);
+    @GET("clientes/{id}")
+    Call<Cliente> getByIdCliente(@Path("id") Long id);
+    @POST("clientes")
+    Call<Cliente> criarCliente(@Body Cliente cliente);
+
+
+
 }
